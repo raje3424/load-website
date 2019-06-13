1064c6d0eaf0c99202df1435a6c8bb6a5f3e1580
Raje Onkar Narendra <raje.onkar@gmail.com>
2019-06-13 17:26:03 +0530

    done

--- src/_service/http.service.ts
+++ src/_service/http.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers } from '@angular/http';
-import { Observable } from 'rxjs';
-
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/toPromise';
-
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +12,10 @@ import 'rxjs/add/operator/toPromise';
 
 export class HttpService {
 
-  private configUrl:any = 'http://192.168.0.17:4447/';
+  private configUrl:any = 'http://localhost:8888/interface.php';
   private headers = new Headers({'Content-Type': 'application/json'});
+
+  private data = new BehaviorSubject('');
 
   private handleError(error: any): Promise<any> {
     console.error(error);
@@ -25,9 +26,10 @@ export class HttpService {
 
   saveEnquiry(value:any): Promise<any> {
     console.log(value);
-    let data = {
-      "function": "Hello",
-      "args" : value
+    let dataValue = {
+      "v_class": "palika",
+      "v_funct": "enquiry",
+      "value" : value
     }
 
     let headers = new Headers({ 
@@ -35,16 +37,100 @@ export class HttpService {
       "Accept": "text/plain" 
     });
 
-    return this.http.post(this.configUrl, value, {headers: this.headers})
+    return this.http.post(this.configUrl, dataValue, {headers: headers})
         .toPromise()
         .then(resp => resp.json())
-        .catch(this.handleError);
+        .catch(this.handleErrorObservable);
+  }
+
+  logIn(value:any): Promise<any> {
+    console.log(value);
+    let dataValue = {
+      "v_class": "palika",
+      "v_funct": "login",
+      "value" : value
+    }
+
+    let headers = new Headers({ 
+      'Content-Type': 'application/json',
+      "Accept": "text/plain" 
+    });
+
+    return this.http.post(this.configUrl, dataValue, {headers: headers})
+        .toPromise()
+        .then(resp => resp.json())
+        .catch(this.handleErrorObservable);
+
+        // let headers = new Headers({ 'Content-Type': 'application/json', "Accept": "text/plain" });
+        // let options = new RequestOptions({ headers: headers });
+        // return this.http.post(this.configUrl, dataValue, options)
+        //     .map(res => {
+        //         // login successful if there's a jwt token in the response
+        //         if (res) {
+        //             // store user details and jwt token in local storage to keep user logged in between page refreshes
+        //             localStorage.setItem('token', JSON.stringify(res.token));
+        //         }
+        //         return res;
+        //     });
+  
+  }
+
+  signIn(value:any): Promise<any> {
+    console.log(value);
+    let dataValue = {
+      "v_class": "palika",
+      "v_funct": "signUp",
+      "value" : value
+    }
+
+    let headers = new Headers({ 
+      'Content-Type': 'application/json',
+      "Accept": "text/plain" 
+    });
+
+    return this.http.post(this.configUrl, dataValue, {headers: headers})
+        .toPromise()
+        .then(resp => resp.json())
+        .catch(this.handleErrorObservable);
+  }
+
+  logOut(){
+    let dataValue = {
+      "v_class": "palika",
+      "v_funct": "logout",
+      "value": {
+        "token": localStorage.getItem('token')
+      }
+    }
+
+    let headers = new Headers({ 
+      'Content-Type': 'application/json',
+      "Accept": "text/plain" 
+    });
+
+    return this.http.post(this.configUrl, dataValue, {headers: headers})
+        .toPromise()
+        .then(resp => resp.json())
+        .catch(this.handleErrorObservable);
+  }
+
+  fetchData(dataValue){
+    let headers = new Headers({ 
+      'Content-Type': 'application/json',
+      "Accept": "text/plain" 
+    });
+
+    return this.http.post(this.configUrl, dataValue, {headers: headers})
+        .toPromise()
+        .then(resp => resp.json())
+        .catch(this.handleErrorObservable);
   }
 
   private extractData(res: Response) {
     let body = res.json();
     return body || {};
-}
+  }
+
 
   private handleErrorObservable (error: Response | any) {
     console.error(error);
--- src/app/requirement-module/landing-page/landing-page.component.ts
+++ src/app/requirement-module/landing-page/landing-page.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, FormBuilder, Validators, MinLengthValidator } f
 import { Router } from '@angular/router';
 
 import { CommunicationService } from '../../../_service/communication.service';
+import { HttpService } from '../../../_service/http.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -19,7 +20,7 @@ export class LandingPageComponent implements OnInit {
   successMsg: any;
   errorMsg: any;
 
-  constructor(private formBuilder: FormBuilder, private _router: Router, private _comService: CommunicationService) {
+  constructor(private formBuilder: FormBuilder, private _router: Router, private _comService: CommunicationService, private _apiService: HttpService) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
@@ -72,71 +73,71 @@ export class LandingPageComponent implements OnInit {
 
   logIn(){
     console.log('Login');
-    // console.warn(this.loginForm.value);
-    // this._apiService.logIn(this.loginForm.value)
-    // .then(resp => {
-    //   console.log(resp);
-    //   if(resp.response == 'true'){
-    //     if(resp.adminFlag == 1){
-    //       this.successMsg = 'Wait...!';
-    //       setTimeout(()=>{
-    //         this.loginForm.reset();
-    //         this.successMsg = '';
-    //         localStorage.setItem('token', JSON.stringify(resp.token));
-    //         this._comService.showLogoutButtonMethod();
-    //         this._comService.whoIsLoggedMethod('Admin');
-    //         this._router.navigateByUrl('adminHome');
-    //       }, 1500);
-    //     }else {
-    //       this.successMsg = 'Wait...!';
-    //       setTimeout(()=>{
-    //         this.loginForm.reset();
-    //         this.successMsg = '';
-    //         localStorage.setItem('token', JSON.stringify(resp.token));
-    //         this._comService.showLogoutButtonMethod();
-    //         this._comService.whoIsLoggedMethod('Affiliate');
-    //         this._router.navigateByUrl('affHome');
-    //       }, 1500);
-    //     }
-    //   }else  if(resp.response == 'rfalse'){
-    //     this.errorMsg = resp.errMessage;
-    //     setTimeout(()=>{
-    //       this.errorMsg = '';
-    //     }, 2000);
-    //   }else {
-    //     {
-    //       this.errorMsg = resp.errMessage;
-    //       setTimeout(()=>{
-    //         this.errorMsg = '';
-    //       }, 2000);
-    //     }
-    //   }
-    //});
+    console.warn(this.loginForm.value);
+    this._apiService.logIn(this.loginForm.value)
+    .then(resp => {
+      console.log(resp);
+      if(resp.response == 'true'){
+        if(resp.adminFlag == 1){
+          this.successMsg = 'Wait...!';
+          setTimeout(()=>{
+            this.loginForm.reset();
+            this.successMsg = '';
+            localStorage.setItem('token', JSON.stringify(resp.token));
+            this._comService.showLogoutButtonMethod();
+            this._comService.whoIsLoggedMethod('Admin');
+            this._router.navigateByUrl('adminHome');
+          }, 1500);
+        }else {
+          this.successMsg = 'Wait...!';
+          setTimeout(()=>{
+            this.loginForm.reset();
+            this.successMsg = '';
+            localStorage.setItem('token', JSON.stringify(resp.token));
+            this._comService.showLogoutButtonMethod();
+            this._comService.whoIsLoggedMethod('Affiliate');
+            this._router.navigateByUrl('affHome');
+          }, 1500);
+        }
+      }else  if(resp.response == 'rfalse'){
+        this.errorMsg = resp.errMessage;
+        setTimeout(()=>{
+          this.errorMsg = '';
+        }, 2000);
+      }else {
+        {
+          this.errorMsg = resp.errMessage;
+          setTimeout(()=>{
+            this.errorMsg = '';
+          }, 2000);
+        }
+      }
+    });
   }
 
   signIn(){
     console.log('signup');
-    // console.warn(this.signupForm.value);
-    // if(this.signupForm.controls['password'].value === this.signupForm.controls['confPassword'].value){
-    //   this._apiService.signIn(this.signupForm.value)
-    //   .then(resp => {
-    //     console.log(resp);
-    //     if(resp.response== 'true'){
-    //       this.successMsg = 'Registered Successfully!';
-    //       setTimeout(()=>{
-    //         this.signupForm.reset();
-    //         this.successMsg = '';
-    //       }, 1500);
-    //     }else {
-    //       this.errorMsg = resp.errMessage;
-    //       setTimeout(()=>{
-    //         this.errorMsg = '';
-    //       }, 2000);
-    //     }
-    //   });
-    // }else {
-    //   alert('Password & confirm password should be same!');
-    // }
+    console.warn(this.signupForm.value);
+    if(this.signupForm.controls['password'].value === this.signupForm.controls['confPassword'].value){
+      this._apiService.signIn(this.signupForm.value)
+      .then(resp => {
+        console.log(resp);
+        if(resp.response== 'true'){
+          this.successMsg = 'Registered Successfully!';
+          setTimeout(()=>{
+            this.signupForm.reset();
+            this.successMsg = '';
+          }, 1500);
+        }else {
+          this.errorMsg = resp.errMessage;
+          setTimeout(()=>{
+            this.errorMsg = '';
+          }, 2000);
+        }
+      });
+    }else {
+      alert('Password & confirm password should be same!');
+    }
     
   }
 
